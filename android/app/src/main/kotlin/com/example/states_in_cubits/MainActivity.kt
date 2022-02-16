451520515159fb2cbd21558356b6c1e964b3dea5
Vijay <vijaythevirtue@gmail.com>
2022-02-16 16:23:35 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/states_in_cubits/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.states_in_cubits
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'Presentation/Routes/route_generator.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      onGenerateRoute: RouteGenerator.generateRoute,
+    );
+  }
+}
